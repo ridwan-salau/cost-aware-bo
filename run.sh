ee385b56d47047168cf713dbd130ab44d66541cc
Ridwan Salahuddeen <ridzy619@yahoo.com>
2023-05-04 15:17:05 +0400

    Add environment yml file

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ ACQF_ARRAY=("EEIPU")
 
 for acqf in ${ACQF_ARRAY[@]}; do
     for trial in {1..1}; do
-        /home/ridwan/miniconda3/envs/tuun/bin/python main.py --trial-num $trial --exp-group $exp_group --acqf $acqf &
+        python main.py --trial-num $trial --exp-group $exp_group --acqf $acqf &
     done;
 done;
 
