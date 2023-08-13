8df2697e2e715166d5bd5fecd6893b8fe20c5dd5
Ridwan Salahuddeen <ridzy619@yahoo.com>
2023-08-13 05:57:13 +0400

    Update latest changes

---
+++ melanoma_bo_trials.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+EXP_GROUP=HPCLAB
+ACQF_ARRAY=("EI")
+MODE="${1:-online}"
+
+# Function to execute the Python script
+run_python_script() {
+    local GPU_IP=$1
+    local EXP_NAME=$(date +%Y%m%d_%H%M%S)_$RANDOM
+    local DIR=/home/ridwan.salahuddeen/Documents/research
+    local PYTHON=/home/ridwan.salahuddeen/.conda/envs/codeformer/bin/python3
+    ssh "$GPU_IP" "cd $DIR \
+        && conda activate codeformer \
+        && WANDB_MODE=$MODE \
+        $PYTHON cost-aware-bo/melanoma_hparams.py \
+            --trial $trial \
+            --exp-group $EXP_GROUP \
+            --acqf $acqf \
+            --exp-name $EXP_NAME"
+    sleep 2
+}
+
+IN_USE_MACHINES=()
+IN_USE_MACHINES_LOCK=/tmp/in_use_machines.lock
+# prev_used() {
+#     local IN_USE_MACHINES=$1
+#     echo Previously used array $IN_USE_MACHINES >&2
+#     local found=false
+#     for element in "${IN_USE_MACHINES[@]}"; do
+#         if [ "$element" == "$ip" ]; then
+#             found=true
+#             break
+#         fi
+#     done
+#     echo "$found"
+# }
+
+prev_used() {
+    local ip="$1"
+    local IN_USE_MACHINES="$2"
+    for element in "${IN_USE_MACHINES[@]}"; do
+        if [ "$element" == "$ip" ]; then
+            return 0  # Found
+        fi
+    done
+    return 1  # Not found
+}
+
+is_machine_free() {
+    GPU_IP=$1
+    echo Checking if $GPU_IP is available  >&2
+    if echo $(ssh $GPU_IP pgrep -a python) | grep -q "melanoma.py"; then
+        # echo $(ssh $GPU_IP pgrep -a python) >&2
+        return 1
+    else
+        echo $GPU_IP is NOT available >&2
+        return 0
+    fi
+}
+
+get_free_gpu() {
+    local min_memory=$1
+    local end=50
+    local c=11
+    local IN_USE_MACHINES=$2
+    while [ "$c" -lt "$end" ]; do
+        local ip="10.127.30.$c"
+        echo "Checking machine $ip..." >&2
+        gpu=$(ssh -oBatchMode=yes -o ConnectTimeout=5 "$ip" nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits)
+
+        echo "IN_USE_MACHINES contents 2:" >&2
+        for element in "${IN_USE_MACHINES[@]}"; do
+            echo - "$element" >&2
+        done
+        if [ $? -eq 0 ] && [ "$gpu" -gt "$min_memory" ]; then
+            if ! prev_used "$ip" "$IN_USE_MACHINES" && is_machine_free "$ip" ; then
+                echo "Found a large GPU: $ip" >&2
+                echo "$ip"
+                break
+            fi
+
+        fi
+
+        echo "GPU $ip has $gpu Mb available; Expecting $min_memory" >&2
+
+        if [ "$c" -eq $((end-1)) ]; then
+            echo "starting search for free GPU all over" >&2
+            c=11
+        fi
+        ((c++))
+    done
+}
+
+
+
+# Function to atomically update IN_USE_MACHINES array
+update_in_use_machines() {
+    local new_ip="$1"
+    echo "Adding $new_ip to used machines array" 
+    flock "$IN_USE_MACHINES_LOCK" -c "echo $new_ip >> IN_USE_MACHINES"
+    echo After adding ... >&2
+    for element in "${IN_USE_MACHINES[@]}"; do
+        echo - "$element"  >&2
+    done
+}
+
+min_required_memory=20000
+
+trial=$2
+gpu_ip=$3
+# while [ "$trial" -le 10 ]; do
+    for acqf in "${ACQF_ARRAY[@]}"; do
+        echo Before getting the gpu_ip ...
+        for element in "${IN_USE_MACHINES[@]}"; do
+            echo - "$element" 
+        done
+        # gpu_ip=$(get_free_gpu "$min_required_memory" "$IN_USE_MACHINES")
+        
+
+        if [ -n "$gpu_ip" ]; then
+            # update_in_use_machines "$gpu_ip"
+            # IN_USE_MACHINES+=("$gpu_ip")
+            run_python_script "$gpu_ip" 
+            # sleep 4
+        # else
+        #     echo "No available GPU found. Waiting..."
+        #     sleep 10
+        fi
+    done
+
+    # ((trial++))
+# done
+
+wait  # Wait for all remaining background processes to finish
+# find ~/Documents/research/input/preprocessed/ -type f -print0 | xargs -0 -n1 -P20 rm
--- melanoma_hparams.py
+++ melanoma_hparams.py
@@ -1,30 +1,22 @@
+import copy
 import json
 import random
-import subprocess
-import sys
-import time
-from copy import deepcopy
+import os
 from argparse import ArgumentParser
-from collections import OrderedDict
+import time
+from typing import List, Union, Dict
 from pathlib import Path
-from typing import Dict, List, Union
-
-import yaml
-
-sys.path.append("../codeformer/")
+import subprocess
+from copy import deepcopy
 
 import botorch
-import numpy as np
 import torch
+import numpy as np
 import wandb
-from basicsr.utils.misc import get_time_str
-from basicsr.utils.options import parse
-from functions import get_dataset_bounds
-from optimizer.optimize_acqf_funcs import (_optimize_acqf_batch,
-                                           gen_batch_initial_conditions,
-                                           gen_candidates_scipy, optimize_acqf)
-from single_iteration import bo_iteration, read_json
 
+from single_iteration import bo_iteration, read_json
+from optimizer.optimize_acqf_funcs import optimize_acqf, _optimize_acqf_batch, gen_candidates_scipy, gen_batch_initial_conditions
+from functions import get_dataset_bounds
 
 # TODO: Define bounds for hyperparameter values that are bounded
 def sample_value(
@@ -50,94 +42,12 @@ def clip(data:Union[List, int, float], lower:Union[int, float]=None, upper:Union
         return [clip(val, lower, upper) for val in data]
     return max(lower, min(upper, data))
 
+def write_hparams(hparams, filename):
+    with open(filename, "w") as f:
+        json.dump(hparams, f)
 
-def generate_update_stage_hparams(hp: List, hp_path):
-    hp_path = Path(hp_path)
-    with hp_path.open("r") as f:
-        opt: OrderedDict = parse(hp_path, "../codeformer")
-        
-    optimizable: OrderedDict = opt.get("optimizable")
-    hparams: OrderedDict = opt["train"]
-    
-    stg_hp, stg_bounds = [], []
-    hparams_out = {}
-    for key1, value1 in optimizable.items():
-        if isinstance(value1, dict):
-            for key2, value2 in value1.items():
-                lower, upper = value2
-        else:
-            lower, upper = value1
-        
-        dtype = type(lower)
-        if hp:
-            val = clip(dtype(hp.pop(0)), lower, upper)
-        else:
-            val = sample_value(lower, upper, dtype=dtype)
-        
-        stg_hp.append(val)
-        stg_bounds.append([lower, upper])
-        if isinstance(value1, dict):
-            hparams[key1][key2] = val
-        else:
-            hparams[key1] = val
-                
-    opt["train"] = hparams
-    
-    # 
-    hp_out_path = OPTIONS_OUT / hp_path.name[hp_path.name.find("stage"):]
-    hp_out_path.parent.mkdir(parents=True, exist_ok=True) if hp_out_path.name=="stage1.yml" else None
-    opt_dict = json.loads(json.dumps(opt))
-    with hp_out_path.open("w") as f:
-        yaml.dump(opt_dict, f, sort_keys=False)
-    return stg_hp, stg_bounds, hp
-
-
-def generate_hparams(hp:List=None, hp_stg_paths:List=None):
-    '''When a new set of hyperparaters, hp, is provided from the bayesian optimizer, this function saves the hyperparameter values for
-    each stage in the respective files. When hp is none (i.e. when generating the warm up values before running BO), the function for 
-    generating the hps for each stage will randomly sample from a range of values
-    
-    hp_stg_paths: list of paths where stage hyperparameters are stored. It must be arranged in the run right order
-    '''
-    new_x, bounds = [], []
-    hp = hp[:] if hp else hp
-    for stg_idx, stg_path in enumerate(hp_stg_paths):
-        stg_hp, stg_bounds, hp = generate_update_stage_hparams(hp, stg_path)
-        new_x.extend(stg_hp)
-        bounds.append(stg_bounds)
-        # hp_out_paths.append(hp_out_path)
-        
-    return new_x, list(zip(*bounds))
-
-
-def read_stg_costs(stage_costs_outputs):
-    # stage_costs = ["stage1_cost.json", "stage2_cost.json", "stage3_cost.json"]
-    new_stg_costs  = [] 
-    for i in range(len(stage_costs_outputs)):
-        cost = stage_costs_outputs[i]
-        if not cost.exists():
-            print(f"Cannot find {cost}")
-            if i>1:
-                raise FileNotFoundError(f"Cannot find {cost}")
-            break 
-        with cost.open() as stg:
-            stg_json = json.load(stg)
-            new_stg_costs.append(stg_json["COST"])
-            
-    return new_stg_costs
-
-def read_objective(obj_path):
-    # obj_path = Path(METRICS_PATH)/"stage3_obj.json"
-    if not obj_path.exists():
-        raise FileNotFoundError(f"File {obj_path} not found.")
-    with obj_path.open() as f:
-        return json.load(f)["OBJ"]
-
-def read_hp_dataset(hp_dataset_path):
-    # hp_dataset_path = Path(METRICS_PATH)/"hp_dataset.json"
-    if not hp_dataset_path.exists():
-        return {"x":[], "y":[], "c":[]}
-    with hp_dataset_path.open() as f:
+def read_hparams(filename):
+    with open(filename) as f:
         return json.load(f)
 
 def write_dataset(dataset):
@@ -146,6 +56,91 @@ def write_dataset(dataset):
         for key, val in dataset.items():
             dataset[key] = val.tolist()# if isinstance(val, torch.Tensor) else val
         json.dump(dataset, f, sort_keys=True)
+    
+def generate_update_stage_hparams(hp: List, hp_stg, stage_idx):
+    
+    stg_hp, stg_bounds = [], []
+    # hparams_out = {}
+    # for sub_stage, sub_hparams in hparams[stage_idx].items():
+    #     tunable_param_keys = list(filter(lambda k: k.startswith("__"), sub_hparams))
+    #     main_params = dict(filter(lambda k: not k[0].startswith("__"), sub_hparams.items()))
+    for key, val in hp_stg.items():
+        if val.get("range"):
+            lower, upper = val["range"]
+            assert type(lower) == type(upper), "lower and upper values must be of same type"
+            dtype = type(lower)
+        else: # the other option should be "choice" but that has not been implemented
+            raise f"{val.keys()} not implemented as an option yet"
+        
+        if hp:
+            val = clip(dtype(hp.pop(0)), lower, upper)
+        else:
+            val = sample_value(lower, upper, dtype=dtype)
+
+        stg_hp.append(val)
+        stg_bounds.append([lower, upper])    
+    
+    # for stg_idx in range(stage_idx):
+    #     stage_params = hparams[stg_idx]
+    #     stage_params_filtered = {}
+    #     for sub_stage, sub_hparams in stage_params.items():
+    #         main_params = dict(filter(lambda k: not k[0].startswith("__"), sub_hparams.items()))
+    #         stage_params_filtered[sub_stage] = main_params
+    #     hparams[stg_idx] = stage_params_filtered
+        
+    # hparams[stage_idx] = hparams_out
+    # hp_out_path = Path("/home/ridwan/workdir/cost_aware_bo/inputs") / hp_path.name
+    # write_hparams(hparams, hp_out_path)
+    return stg_hp, stg_bounds, hp
+
+def generate_hparams(hp:List=None, hp_stgs:List=None):
+    '''When a new set of hyperparaters, hp, is provided from the bayesian optimizer, this function saves the hyperparameter values for
+    each stage in the respective files. When hp is none (i.e. when generating the warm up values before running BO), the function for 
+    generating the hps for each stage will randomly sample from a range of values
+    
+    hp_stgs: list of paths where stage hyperparameters are stored. It must be arranged in the run right order
+    '''
+    new_x, bounds, model_input = [], [], []
+    hp = hp[:] if hp else hp
+    for stg_idx, hp_stg in enumerate(hp_stgs):
+        out_hp_stg, stg_bounds, hp = generate_update_stage_hparams(hp, hp_stg, stg_idx)
+        new_x.extend(out_hp_stg)
+        bounds.append(stg_bounds)
+        model_input.append(out_hp_stg)
+        
+    return new_x, list(zip(*bounds)), model_input
+    
+def read_stg_costs(stage_costs_outputs):
+    new_stg_costs  = [] 
+    stg_json = {}
+    for i in range(len(stage_costs_outputs)):
+        if not os.path.exists(stage_costs_outputs[i]):
+            print(f"Cannot find {stage_costs_outputs[i]}")
+            if i>1:
+                raise FileNotFoundError(f"Cannot find {stage_costs_outputs[i]}")
+            break 
+        with open(stage_costs_outputs[i]) as stg:
+            stg = stg.read().strip()
+            print("param_gen 125", stg, stage_costs_outputs[i], end="\n\n")
+            stg_json = json.loads(stg[:stg.find("}")+1] if "}" in stg else f"{stg}{'}'}")
+            new_stg_costs.append(stg_json["COST"])
+            
+    return new_stg_costs
+
+def read_objective(obj_output):
+    if not Path(obj_output).exists():
+        print(obj_output)
+        raise FileNotFoundError(f"File {obj_output} not found.")
+    with open(obj_output) as f:
+        obj = f.read()
+        print(obj, obj_output)
+        return json.loads(obj).get("OBJ",float("inf"))
+
+def read_hp_dataset(HP_DATASET):
+    if not os.path.exists(HP_DATASET):
+        return {"x":[], "y":[], "c":[]}
+    with open(HP_DATASET) as f:
+        return json.load(f)
 
 def generate_hps(
     iteration,
@@ -155,11 +150,11 @@ def generate_hps(
         0:"./stage_zero_cost.json", 1:"./stage_one_cost.json", 2:"./stage_two_cost.json",
     },
     obj_output: str = "./score.json",
-    hp_stg_paths = [], # The number of paths provided should match the number of stages
+    hp_stgs = [], # The number of paths provided should match the number of stages
     # config_file:Path = Path("")
 ):
     tic = time.time()
-    bo_params = read_json(BASE_DIR / TUUN_DIR / "params")
+    bo_params = read_json(BASE_DIR/TUUN_DIR/"params-melanoma")
     dtype = torch.double
     device = "cuda" if torch.cuda.is_available() else "cpu"
     
@@ -178,27 +173,23 @@ def generate_hps(
     
     # When new_x is None, `generate_hparams` will generate random samples.
     # It also saves the new_x to the respective files where the main function can read them 
-    new_x, x_bounds = generate_hparams(new_x, hp_stg_paths)
+    new_x, x_bounds, model_input = generate_hparams(new_x, hp_stgs)
     
     ## Execute model (PIRLib)
     
     # resp = subprocess.run(["python", "stacking_algo.py"], capture_output=True, text=True) # For testing
     keys = stage_costs_outputs.keys()
-    # stage_costs_outputs_list = [stage_costs_outputs[i] for i in keys]
-    # print("stage_costs_outputs_list", stage_costs_outputs_list)
-    
-    cmd = ["bash", "codeformer/run_codeformer.sh", EXP_NAME, METRICS_PATH]
-    # cmd = ["argo", "submit", "-n", "gpu-14", "--wait", config_file],
+    cmd = ["python3", "./melanoma/melanoma.py", "--hps", *[str(i) for i in new_x], "--metrics-path", str(METRICS_PATH)]
+    print("Executing...", " ".join(cmd))
     cmd.append("--disable-cache") if acq_type=="EI" else None
-    resp = subprocess.run(cmd, capture_output=True, text=True)
-    print(resp.stdout)
-    print("Done running model")
-    print(resp.stderr)
-    # if resp.stderr:
-    #     raise Exception(resp.stderr)
+    resp = subprocess.run(cmd, )
+    
     # print(resp.stdout)
+    print("Done running model")
+    # print(resp.stderr)
     while not Path(obj_output).exists():
         raise FileNotFoundError(obj_output)
+    
     
     def update_dataset_new_run(dataset, new_x, stage_costs_outputs, obj_output, x_bounds, dtype=torch.float64, device="cuda"):
         # Check PIRLIB output directory for new objective and cost values
@@ -222,7 +213,7 @@ def generate_hps(
         return dataset
     
     dataset = update_dataset_new_run(dataset, new_x, stage_costs_outputs, obj_output, x_bounds, dtype, device)
-    best_f = dataset["y"].min().item()
+    best_f = dataset["y"].max().item()
     new_y = dataset["y"][-1].item()
     stage_cost_list = dataset["c"][:, -1].squeeze()
     sum_stages = sum(stage_cost_list)
@@ -267,7 +258,7 @@ def generate_hps(
     wandb.log(log)
     return new_x
 
-if __name__=="__main__":
+if __name__ == "__main__":
     t = time.localtime()
     parser = ArgumentParser()
     parser.add_argument("--trial", type=int, help="The trial number", default=0)
@@ -276,8 +267,8 @@ if __name__=="__main__":
     # parser.add_argument("--obj-output", help="The output directory of the objective.", required=True)
     parser.add_argument("--base-dir", type=Path, help="Base tuun directory.", default=Path("."))
     parser.add_argument("--exp-name", type=str, help="Experiment name.")
-    parser.add_argument("--init-eta", type=float, help="Initial ETA", default=3)
-    parser.add_argument("--decay-factor", type=float, help="Decay factor", default=1)
+    parser.add_argument("--init-eta", type=float, help="Initial ETA", default=1)
+    parser.add_argument("--decay-factor", type=float, help="Decay factor", default=0.95)
     parser.add_argument("--exp-group", type=str, help="Group ID")
     parser.add_argument("--acqf", type=str, help="Acquisition function", choices=["EEIPU", "EIPU", "EIPU-MEMO", "EI", "RAND"], default="EEIPU")
 
@@ -289,21 +280,25 @@ if __name__=="__main__":
     EXP_NAME = args.exp_name
     METRICS_PATH = BASE_DIR / TUUN_DIR / "metrics" / EXP_NAME
     HP_DATASET = METRICS_PATH / "hp_dataset.json"
-    
-    options = "codeformer/options"
-    OPTIONS_OUT = BASE_DIR / options / EXP_NAME
-    hp_stg_paths = [
-        BASE_DIR / options / "VQGAN_512_ds32_nearest_stage1.yml",
-        BASE_DIR / options / "CodeFormer_stage2.yml",
-        BASE_DIR / options / "CodeFormer_stage3.yml"
-    ]
-    
-    stages = ["stage1_cost.json", "stage2_cost.json", "stage3_cost.json"]
+        
+    stages = ["stage1_cost.json", "stage2_cost.json"]
     stage_costs_outputs = {ind:METRICS_PATH/file for ind,file in enumerate(stages)}
     
-    obj_output = METRICS_PATH / "stage3_obj.json"
+    obj_output = METRICS_PATH / "obj.json"
     
-    params = read_json(BASE_DIR / TUUN_DIR / "params")
+    hp_stgs = [
+        dict(
+            bright_limit=dict(range=[0.1, 0.8]), 
+            cont_limit=dict(range=[0.1, 0.8]), 
+            p=dict(range=[0.1, 0.8])), 
+        dict(
+            init_lr=dict(range=[1e-6, 1e-4]),
+            warmup_epo=dict(range=[1, 3]),
+            h1=dict(range=[128, 1024]),
+            h2=dict(range=[32, 512]),
+            drp_out_p=dict(range=[0.1, 0.8]))
+        ]
+    params = read_json(BASE_DIR / TUUN_DIR / "params-melanoma")
     args_dict = deepcopy(vars(args))
     params.update(args_dict)
     
@@ -335,7 +330,7 @@ if __name__=="__main__":
             stage_costs_outputs=stage_costs_outputs,
             obj_output=obj_output,
             # config_file=args.config_file,
-            hp_stg_paths=hp_stg_paths,
+            hp_stgs=hp_stgs,
             acq_type=args.acqf,
         )        
     print("Done!!!")
