eb86634725f88316522e2616977d35c771f9abc3
Ridwan Salahuddeen <ridzy619@yahoo.com>
2023-12-14 18:28:25 -0500

    Minor improvements

--- t5_fine_tuning/run_multi.sh
+++ t5_fine_tuning/run_multi.sh
@@ -9,13 +9,13 @@ num_gpus=$(nvidia-smi --list-gpus | wc -l)
 target_dev=0
 for acqf in ${ACQF_ARRAY[@]}; do
     for trial in {1..5}; do
-        log_dir=log/$acqf/$exp_name && \
+        log_file=log/$acqf/$exp_name"_trial_"$trial && \
         data_dir="${2:-./inputs}" && \
         ((target_dev+=1))
         cache_root=.cachestore/${acqf}/${RANDOM}_trial_${trial} && \
         CUDA_VISIBLE_DEVICES=$target_dev python optimize_multi.py \
             --exp-name $exp_name --trial $trial --cache-root \
-            $cache_root --acqf $acqf --data-dir $data_dir &>> ${log_dir}.log && rm -rf $cache_root
+            $cache_root --acqf $acqf --data-dir $data_dir 2>&1 | tee -a ${log_file}.log && rm -rf $cache_root &
 
         if [ $(($target_dev%$num_gpus)) -eq 0 ]; then
             wait # Wait for the inner loop to complete before continuing
