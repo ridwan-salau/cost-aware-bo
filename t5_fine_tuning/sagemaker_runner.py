19d625376eae3ca438db32073a3ea0495aa9ba04
Ridwan Salahuddeen <ridzy619@yahoo.com>
2024-01-19 13:36:40 -0500

    Revert sagemaker code

--- t5_fine_tuning/sagemaker_runner.py
+++ t5_fine_tuning/sagemaker_runner.py
@@ -12,7 +12,6 @@ region = sagemaker_session.boto_region_name
 bucket = "mbz-hpc-aws-master"
 
 role = sagemaker.get_execution_role()
-print(role)
 
 RANDOM = random.randint(1000, 10000)
 acqf = "EEIPU"
@@ -23,38 +22,19 @@ data_dir=f"{root}/inputs"
 
 cache_root=f"{root}/.cachestore/{acqf}/{RANDOM}_trial_{trial}"
 
-settings = dict(
-    # entry_point="optimize_multi.py",
-    # source_dir="./",
-    sagemaker_session=sagemaker_session,
+estimator = PyTorch(
+    entry_point="optimize_multi.py",
+    source_dir="./",
     role=role,
-    # py_version="py38",
-    # framework_version="1.11.0",
-    instance_count=1,
+    py_version="py38",
+    framework_version="1.11.0",
+    instance_count=2,
     instance_type="ml.c5.2xlarge",
-    # output_path=f"s3://{root}",
-    # hyperparameters={"exp-name": exp_name, "trial": trial, "data-dir": data_dir, "acqf": acqf, "cache-root": cache_root},
+    output_path=f"s3://{root}",
+    hyperparameters={"exp-name": exp_name, "trial": trial, "data-dir": data_dir, "acqf": acqf, "cache-root": cache_root},
 )
 
-@remote(**settings)
-def hello():
-    return ("Hello, world")
-
-if __name__=="__main__":
-    print(hello())
-# estimator = PyTorch(
-#     entry_point="optimize_multi.py",
-#     source_dir="./",
-#     role=role,
-#     py_version="py38",
-#     framework_version="1.11.0",
-#     instance_count=2,
-#     instance_type="ml.c5.2xlarge",
-#     output_path=f"s3://{root}",
-#     hyperparameters={"exp-name": exp_name, "trial": trial, "data-dir": data_dir, "acqf": acqf, "cache-root": cache_root},
-# )
-
-# estimator.fit()
+estimator.fit()
 
 '''
 python optimize_multi.py \
